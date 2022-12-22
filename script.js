a25041c4d7f897ba723d12dc13cd05f1665f42fa
David Andersson <david.andersson@zocom.se>
2022-12-22 11:29:23 +0100

    Added a click count cookie

--- script.js
+++ script.js
@@ -3,26 +3,53 @@ const showCookies = document.querySelector('#show-cookies')
 const cookieOutput = document.querySelector('#cookie-output')
 const welcome = document.querySelector('#welcome')
 
+const lastVisitKey = 'lastVisit'
+const clickCountKey = 'clickCount'
 
 let cookieData = document.cookie
-let position = cookieData.search('lastVisit=')
-if( position === 0 ) {
-	let value = cookieData.substring(10)
-	console.log('Det finns en cookie med value=', value)
-	let lastVisit = new Date(value)
-	let lastVisitMs = lastVisit.getTime()
+let clickCount = 0
+let cookieList = cookieData.split(';')
+welcome.innerText = ''
+cookieList.forEach(cookie => {
+	cookie = cookie.trim()
+	// console.log('Cookie list loop', cookie, cookie.search(lastVisitKey + '='))
+	if (cookie.search(lastVisitKey + '=') === 0 ) {
+		let value = cookie.substring(lastVisitKey.length + 1)
+		// console.log('Det finns en cookie med value=', value)
+		let lastVisit = new Date(value)
+		let lastVisitMs = lastVisit.getTime()
 
-	let timeNowMs = Date.now()
+		let timeNowMs = Date.now()
 
-	let diff = (timeNowMs - lastVisitMs) / 1000
-	console.log('Diff ', timeNowMs, lastVisitMs)
-	welcome.innerText = `Välkommen tillbaka! Du var senast här för ${diff} sekunder sedan.`
+		let diff = (timeNowMs - lastVisitMs) / 1000
+		// console.log('Diff ', timeNowMs, lastVisitMs)
+		welcome.innerText += `Välkommen tillbaka! Du var senast här för ${diff} sekunder sedan.`
+	}
+	else if (cookie.search(clickCountKey + '=') === 0) {
+		let value = cookie.substring(clickCountKey.length + 1)
+		// Obs! value är en sträng och måste typomvandlas
+		clickCount = Number(value)
+		welcome.innerText += `Antal klick: ${clickCount}. `
+	}
+})
 
-} else {
-	console.log('Det finns inte någon cookie')
-}
+// Typomvandling:
+// -> datatypens funktion, exempel Number('15'), Boolean('true')
+// -> automatisk typkonvertering: 1 + '1' === '1' + '1' === '11'
+// -> '15' - '10' === 15 - 10 === 5
+// -> +'25' === 25
+
+// Viktiga metoder för stränghantering:
+// - substring
+// - search, includes
+// - split
+// - trim, toLowerCase, toUpperCase
+
+
 let dateTimeString = getISOTime()
-document.cookie = `lastVisit=${dateTimeString}`
+document.cookie = `${lastVisitKey}=${dateTimeString}`
+
+
 // TODO: använd konstanter i stället för "magic strings" som 'lastVisit='
 
 // const str = 'aefghijklmnopqr'
@@ -36,7 +63,9 @@ document.cookie = `lastVisit=${dateTimeString}`
 
 addCookie.addEventListener('click', () => {
 	let dateTimeString = getISOTime()
-	document.cookie = `lastVisit=${dateTimeString}`
+	document.cookie = `${lastVisitKey}=${dateTimeString}`
+	clickCount++
+	document.cookie = `clickCount=${clickCount}` // 'clickCount=' + clickCount
 })
 
 showCookies.addEventListener('click', () => {
